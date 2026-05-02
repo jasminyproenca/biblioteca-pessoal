773c1cde403a35ca0964d59e4b3d8ebfad1ac10f
Jasminy <jasminyproenca@gmail.com>
2026-05-02 11:00:52 -0300

    Implementando Relatorio

--- gestao-biblioteca-testes-web/cypress.config.js
+++ gestao-biblioteca-testes-web/cypress.config.js
@@ -1,11 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
+    baseUrl: 'http://localhost:5173',
+    reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on)
     },
   },
 });
